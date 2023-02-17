01372ad40c20e5909eef0bfac0bdfd9bca0cc6d8
jmreal <etiuzaleumoka@yahoo.com>
2023-02-16 21:27:22 -0800

    alx c task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,9 @@ long int age;
 long long int nums;
 float doc;
 	printf("Size of a char: %zu byte(s)\n", sizeof(number));
-	printf("Size of an int; 5i byte(s)\n", sizeof(num));
-	prinf("Size of a long int: %i byte(s)\n", sizeof(age));
-	printf("Size of a long long int %i byte(s)\n", sizeof(nums));
-	printf("Size of a float: %i byte(s)\n", sizeof(doc));
+	printf("Size of an int; %zu byte(s)\n", sizeof(num));
+	prinf("Size of a long int: %zu byte(s)\n", sizeof(age));
+	printf("Size of a long long int %zu byte(s)\n", sizeof(nums));
+	printf("Size of a float: %zu byte(s)\n", sizeof(doc));
 	return (0);
 }
