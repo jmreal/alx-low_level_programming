77d8a99aa49aa81883177622c111399f35c07972
jmreal <etiuzaleumoka@yahoo.com>
2023-02-19 20:39:04 -0800

    alx task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-
 /**
- * main - prints all combination of single digits
- * Return; Always 0
+ * main - prints all possible combinations of single-digit numbers
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
 
-	for(n = 48; n < 58; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
 		if (n != 57)
@@ -16,6 +15,7 @@ int main(void)
 			putchar(',');
 			putchar(' ');
 		}
+	}
 	putchar('\n');
 	return (0);
 }
